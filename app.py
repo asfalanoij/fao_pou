dd0337eac9044589946d8303fd785aab240451c3
asfalanoij <poncoltawangjaya@gmail.com>
2025-05-24 18:27:19 +0700

    UI: Remove gridlines from country trends plot for a cleaner look

--- app.py
+++ app.py
@@ -107,8 +107,8 @@ def plot_country_trends(filtered: pd.DataFrame):
         height=600,
         template="plotly_white",
         font=dict(size=15),
-        xaxis=dict(showgrid=True, gridcolor="#e5e5e5"),
-        yaxis=dict(showgrid=True, gridcolor="#e5e5e5"),
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False),
         legend=dict(font=dict(size=13)),
         margin=dict(l=40, r=40, t=60, b=40)
     )
